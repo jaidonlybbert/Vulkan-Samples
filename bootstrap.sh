a443ffa111b36cb92229f51be712678e9c2019f3
Jaidon Lybbert <jaidonlybbert@gmail.com>
2024-10-03 22:58:15 -0700

    Updated environment config

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,13 @@
 #!bin/bash
 
-export BUILD_PATH=~/src/Vulkan-Samples/build/mac
-export SAMPLES_BIN=~/src/Vulkan-Samples/build/mac/app/bin/Release/arm64/vulkan_samples
+export REPO_ROOT_DIR=~/src/Vulkan-Samples
+export BUILD_PATH=${REPO_ROOT_DIR}/build/mac
+export SAMPLES_BIN=${REPO_ROOT_DIR}/build/mac/app/bin/Release/arm64/vulkan_samples
 export VULKAN_ENV_INIT=~/VulkanSDK/1.3.283.0/setup-env.sh
+export PROJECT_SRC_DIR=${REPO_ROOT_DIR}/samples/general/sandbox/
+export CMAKE_EXPORT_COMPILE_COMMANDS=1
+export VKB_BUILD_SAMPLES=OFF
+export VKB_SANDBOX=ON
 
 install() {
 	source ${VULKAN_ENV_INIT}	
@@ -11,12 +16,13 @@ install() {
 buildtree() {
 	mkdir -p ${BUILD_PATH}
 	rm -rf ${BUILD_PATH}/*
-	cmake -Bbuild/mac -DCMAKE_BUILD_TYPE=Release
-	cmake --build build/mac --config Release --target vulkan_samples -j4
+	cmake -B${BUILD_PATH} -DCMAKE_BUILD_TYPE=Release -G Ninja
+	cmake --build ${BUILD_PATH} --config Release --target vulkan_samples -j4
+	compdb -p ${BUILD_PATH} list > ${REPO_ROOT_DIR}/build/compile_commands.json
 }
 
 build() {
-	cmake --build build/mac --config Release --target vulkan_samples -j4
+	cmake --build ${BUILD_PATH} --config Release --target vulkan_samples -j4
 }
 
 samples() {
@@ -33,4 +39,12 @@ test() {
 	else
 		echo "n"
 	fi
+}
+
+in() {
+	cd ${PROJECT_SRC_DIR}
+}
+
+out() {
+	cd ${REPO_ROOT_DIR}
 }
