e3e40a28e062e99ca8c814b95d272ac3ba939001
Jaidon Lybbert <jaidonlybbert@gmail.com>
2024-10-12 13:06:42 -0700

    Refactor build script, enabled testing

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,8 +6,6 @@ export SAMPLES_BIN=${REPO_ROOT_DIR}/build/mac/app/bin/Release/arm64/vulkan_sampl
 export VULKAN_ENV_INIT=~/VulkanSDK/1.3.283.0/setup-env.sh
 export PROJECT_SRC_DIR=${REPO_ROOT_DIR}/samples/general/sandbox/
 export CMAKE_EXPORT_COMPILE_COMMANDS=1
-export VKB_BUILD_SAMPLES=OFF
-export VKB_SANDBOX=ON
 
 install() {
 	source ${VULKAN_ENV_INIT}	
@@ -16,17 +14,7 @@ install() {
 buildtree() {
 	mkdir -p ${BUILD_PATH}
 	rm -rf ${BUILD_PATH}/*
-	cmake -B${BUILD_PATH} -DCMAKE_BUILD_TYPE=Release -G Ninja
-	cmake --build ${BUILD_PATH} --config Release --target vulkan_samples -j4
-	compdb -p ${BUILD_PATH} list > ${REPO_ROOT_DIR}/build/compile_commands.json
-}
-
-build() {
-	cmake --build ${BUILD_PATH} --config Release --target vulkan_samples -j4
-}
-
-samples() {
-	${SAMPLES_BIN} $1
+	cmake -B${BUILD_PATH} -DCMAKE_BUILD_TYPE=Debug -G Ninja -DVKB_BUILD_TESTS=ON
 }
 
 start() {
@@ -36,12 +24,20 @@ start() {
 	cd ${TMPDIR}
 }
 
-test() {
-	if [ "$1" = "rebuild" ]; then
-		echo "j"
-	else
-		echo "n"
+build() {
+	if [ "$2" = "rebuild" ]; then
+		buildtree
 	fi
+
+	if [ "$1" = "samples" ]; then
+		cmake --build ${BUILD_PATH} --config Debug --target vulkan_samples -j4
+	elif [ "$1" = "tests" ]; then
+		cmake --build ${BUILD_PATH} --config Debug --target vkb__tests -j4
+	elif [ "$1" = "components" ]; then
+		cmake --build ${BUILD_PATH} --config Debug --target vkb__components -j4
+	fi
+
+	compdb -p ${BUILD_PATH} list > ${REPO_ROOT_DIR}/build/compile_commands.json
 }
 
 in() {
