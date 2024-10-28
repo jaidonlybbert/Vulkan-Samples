f7f9c472ef1ccba3fe2f8ec6803b7afe014e9ff3
Jaidon Lybbert <jaidonlybbert@gmail.com>
2024-10-27 20:47:39 -0700

    Added sandbox example

--- samples/general/sandbox/sandbox.cpp
+++ samples/general/sandbox/sandbox.cpp
@@ -2,8 +2,7 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  *
- * Licensed under the Apache License, Version 2.0 the "License";
- * you may not use this file except in compliance with the License.
+ * Licensed under the Apache License, Version 2.0 the "License"; you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
@@ -16,7 +15,6 @@
  */
 
 #include "sandbox.h"
-
 #include "common/vk_common.h"
 #include "gltf_loader.h"
 #include "gui.h"
@@ -24,9 +22,12 @@
 #include "platform/platform.h"
 #include "rendering/subpasses/forward_subpass.h"
 #include "stats/stats.h"
+#include "core/util/logging.hpp"
 
 sandbox::sandbox()
 {
+	LOGI("Something");
+	LOGI("Something else");
 }
 
 bool sandbox::prepare(const vkb::ApplicationOptions &options)
@@ -37,11 +38,23 @@ bool sandbox::prepare(const vkb::ApplicationOptions &options)
 	}
 
 	// Load a scene from the assets folder
-	load_scene("scenes/sponza/Sponza01.gltf");
+	// load_scene("scenes/sponza/Sponza01.gltf");
+	load_scene("scenes/Sandbox/sandbox.gltf");
 
 	// Attach a move script to the camera component in the scene
-	auto &camera_node = vkb::add_free_camera(get_scene(), "main_camera", get_render_context().get_surface_extent());
-	auto camera       = &camera_node.get_component<vkb::sg::Camera>();
+	// auto &camera_node = vkb::add_free_camera(get_scene(), "main_camera", get_render_context().get_surface_extent());
+	// auto camera       = &camera_node.get_component<vkb::sg::Camera>();
+	vkb::sg::Camera* camera = nullptr;
+
+	// Attach a move script to a player character with attached camera
+	auto &player_node = vkb::add_player_controller(get_scene(), "player_1", get_render_context().get_surface_extent());
+	auto camera_pivot = vkb::find_child_by_name(&player_node, "camera_pivot");
+	camera = &vkb::find_child_by_name(camera_pivot, "player_camera")->get_component<vkb::sg::Camera>();
+
+	if (!camera) {
+		LOGE("No camera found for `%`", player_node.get_name());
+		throw std::runtime_error("No camera found for PlayerController: " + player_node.get_name());
+	}
 
 	// Example Scene Render Pipeline
 	vkb::ShaderSource vert_shader("base.vert");
